dae787210e13527dd7edafd1d17308f86ddec04d
Quang-Huy Nguyen <30990121+huynguyen250896@users.noreply.github.com>
2021-01-26 17:30:43 +0700

    Update GeneCluster.R

--- R/GeneCluster.R
+++ R/GeneCluster.R
@@ -10,21 +10,26 @@
 #'
 #' @export
 
-# Compute mean
-SubtypeSpecificGene = function(omics = NULL, cluster = NULL){
+SubtypeSpecificGene = function(omics = NULL, cluster = NULL, adjustedP = T){
   #library
   library(maps)
   library(purrr)
   library(tidyr)
   library(tidyverse)
   library(dplyr)
-
+  
+  #define the computeQ function may adjust gained P-values following Benjamini-Hochberg FDR
+  computeQ <- function(x)
+  {
+    (x$P.Value*nrow(x))/(x$rank)
+  }
+  
   #subtype
   group = data.frame(cluster)
   data = omics %>% data.frame()
   data = data[rownames(group),]
   data$sub = group$cluster
-
+  
   #implementation
   Mean <- round(aggregate(as.matrix(data[,-length(data)]) ~ data[,length(data)], FUN = mean),3)
   Mean=t(Mean)
@@ -33,40 +38,48 @@ SubtypeSpecificGene = function(omics = NULL, cluster = NULL){
   #compute p-value
   lis_out <- data[,-length(data)] %>% purrr::map(~ aov(.x ~ data[,length(data)] )) %>% purrr::map(~summary(.))
   Pvalue_lis<- lis_out %>% purrr::map(~.x[[1]][5] %>% unlist)
-
+  
   Pvalue_out <- do.call(rbind, Pvalue_lis) %>% as.data.frame()
   colnames(Pvalue_out)[1] = "P.Value"
   #merge mean and p-value
   mean_overall = data.frame(Mean,Pvalue_out[1])
-  #create sub matrix with adj.p.val<0.05 ~ GOOD statistically significant difference
-  sig = subset(mean_overall, P.Value <0.05)
-
+  
+  if(adjustedP == T){
+    mean_overall = mean_overall[order(mean_overall$P.Value),] #order rows following p-value
+    mean_overall$rank = rank(mean_overall$P.Value)
+    mean_overall$Q.value = computeQ(mean_overall)
+    #create sub matrix with adj.P.Value =< 0.05 and Q.value =< 0.05 
+    sig = subset(mean_overall, P.Value <= 0.05 & Q.value <= 0.05)
+    sig = sig %>%
+      dplyr::select(-rank)
+  } else{
+    #create sub matrix with P.Value =< 0.05 
+    sig = subset(mean_overall, P.Value <= 0.05)
+  }
+  
   #find specific group of each gene
   sig1 = sig %>%
-    mutate_all(as.numeric) %>%
-    mutate(row = row_number()) %>%
-    pivot_longer(-row) %>%
-    group_by(row) %>%
-    mutate(max_value = max(value),
-           group_number = str_extract(name,"[:digit:]") %>% as.numeric(),
-           GeneCluster = if_else(value == max_value ,group_number,NA_real_)) %>%
-    fill(GeneCluster,.direction = c("updown")) %>%
-    select(-group_number,-max_value) %>%
-    pivot_wider(names_from = name,values_from = value)
+    dplyr::mutate_all(as.numeric) %>%
+    dplyr::mutate(row = row_number()) %>%
+    tidyr::pivot_longer(-row) %>%
+    dplyr::group_by(row) %>%
+    dplyr::mutate(max_value = max(value),
+           group_number = stringr::str_extract(name,"[:digit:]") %>% as.numeric(),
+           GeneCluster = dplyr::if_else(value == max_value ,group_number,NA_real_)) %>%
+    tidyr::fill(GeneCluster,.direction = c("updown")) %>%
+    dplyr::select(-group_number,-max_value) %>%
+    tidyr::pivot_wider(names_from = name,values_from = value)
   #tidy to a dataframe
   sig1 <- as.data.frame(sig1)
   sig1 = sig1[,-1]
   rownames(sig1) = rownames(sig);
-
+  
   #write the results as the csv file
   write.table(sig1,"specific_subtype_gene.txt",sep = "\t", quote = FALSE)
-
+  
   #return the results
   print(head(sig1))
-
+  
   #warning
-  print(writeLines("\nNOTE: \n*the results, which are being shown above, are as incomplete ones.\n*specific_subtype_gene.csv placed in your current working directory.\n*Please check to identify which genes are specifically assigned to which subgroups."))
+  print(writeLines("\nNOTE: \n*the results, which are being shown above, are incomplete.\n*specific_subtype_gene.csv placed in your current working directory.\n*Please check to identify which gene is specifically assigned to which subgroup."))
 }
-
-
-
