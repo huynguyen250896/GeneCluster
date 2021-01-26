95f74ba5b1210829032cbfd636376ffa3f69120a
Quang-Huy Nguyen <30990121+huynguyen250896@users.noreply.github.com>
2021-01-26 23:18:38 +0700

    Update GeneCluster.R

--- R/GeneCluster.R
+++ R/GeneCluster.R
@@ -1,8 +1,22 @@
-#' @title Tool for Identification of subtype-specific genes
+#' @title GeneCluster: Identification of subtype-specific genes
 #'
-#' @description Given a gene from a list of genes of interest, it will be specifically distributed to either of the identified subgroups based on its mean values (e.g., CNA changes, MET changes, and expression levels). Then, a gene was considered as a subtype-specific one if P-value <= 0.05 (one-way ANOVA test). For further information on requirements as well as how to implement this tool, please visit my Github repository: https://github.com/huynguyen250896/GeneCluster
+#' @description A previous study defined subtype-specific genes are the ones mutated predominantly in the samples assigned to one single subtype than in 
+#' the other subtypes [1]. Subsequently, those genes are features that reflect the difference between subgroups of heterogeneous cancers [1, 2]. To 
+#' computationally detect subtype-specific genes, we built the R package GeneCluster from the idea of the reference paper [3]. In brief, given a gene from 
+#' a list of genes of interest, it will be specifically distributed to either of the identified subgroups based on the mean values (e.g., CNA changes, MET 
+#' changes, and expression levels). Then, a gene was considered as a subtype-specific one if P-value <= 0.05 (one-way ANOVA test). 
 #'
-#' @param omics,cluster
+#' @docType package
+#'
+#' @author Quang-Huy Nguyen
+#'
+#' @usage computeC(omics, cluster, adjustedP = T)
+#'
+#' @param omics data.frame or matrix. \code{omics} includes its rows are samples and its columns are genomic features.
+#'
+#' @param cluster numeric. Predictive subgroups correspond with samples after running a clustering tool for your data (e.g., k-means, hclust,...).
+#'
+#' @param adjustedP logical. Whether we should adjust the gained P-values (ANOVA test) using the Benjamini-Hochberg procedure. Default is \code{adjustedP = T}
 #'
 #' @return NULL
 #'
@@ -17,6 +31,19 @@ SubtypeSpecificGene = function(omics = NULL, cluster = NULL, adjustedP = T){
   library(tidyr)
   library(tidyverse)
   library(dplyr)
+  
+  #Errors
+  if(missing(omics)){
+    stop("Error: input data is missing. \n")
+  }
+  
+  if(missing(cluster)){
+    stop("Error: predictive subgroups correspond with samples are missing. \n")
+  }
+  
+  if(!(nrow(omics) != length(clusters))){
+    stop("Error: Please make sure the samples in omics and clusters are the same and in exactly the same order. \n")
+  }
   
   #define the computeQ function may adjust gained P-values following Benjamini-Hochberg FDR
   computeQ <- function(x)
@@ -52,34 +79,75 @@ SubtypeSpecificGene = function(omics = NULL, cluster = NULL, adjustedP = T){
     sig = subset(mean_overall, P.Value <= 0.05 & Q.value <= 0.05)
     sig = sig %>%
       dplyr::select(-rank)
+    
+    #find specific group of each gene
+    sig1 = sig %>%
+      dplyr::mutate_all(as.numeric) %>%
+      dplyr::mutate(row = dplyr::row_number()) %>%
+      tidyr::pivot_longer(-row)
+    
+    sig2 = sig1[!(sig1$name == "P.Value" | sig1$name == "Q.value"),] #temporarily remove P.Value and Q.Value
+    sig2 = sig2 %>% 
+      dplyr::group_by(row) %>%
+      dplyr::mutate(max_value = max(value),
+                    group_number = stringr::str_extract(name,"[:digit:]") %>% as.numeric(),
+                    GeneCluster = dplyr::if_else(value == max_value ,group_number,NA_real_)) %>%
+      tidyr::fill(GeneCluster,.direction = c("updown")) %>%
+      dplyr::select(-group_number,-max_value) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value)
+    P.value = sig$P.Value
+    Q.value = sig$Q.value
+    sig2 = data.frame(sig2, P.value, Q.value)
+    
+    #tidy to a dataframe
+    sig2 <- as.data.frame(sig2)
+    sig2 = sig2[,-1] #remove 'row' column
+    rownames(sig2) = rownames(sig)
+    
+    #write the results as the csv file
+    write.table(sig2,"specific_subtype_gene.txt",sep = "\t", quote = FALSE)
+    
+    #return the results
+    print(head(sig2))
+
+    #warning
+    print(writeLines("\nNOTE: \n*the results shown above are incomplete.\n*specific_subtype_gene.csv placed in your current working directory.\n*Please check to identify which gene is specifically assigned to which subgroup."))
+  
+    
   } else{
     #create sub matrix with P.Value =< 0.05 
     sig = subset(mean_overall, P.Value <= 0.05)
-  }
   
   #find specific group of each gene
   sig1 = sig %>%
     dplyr::mutate_all(as.numeric) %>%
-    dplyr::mutate(row = row_number()) %>%
-    tidyr::pivot_longer(-row) %>%
+    dplyr::mutate(row = dplyr::row_number()) %>%
+    tidyr::pivot_longer(-row)
+
+  sig2 = sig1[!(sig1$name == "P.Value"),] #temporarily remove P.Value
+  sig2 = sig2 %>% 
     dplyr::group_by(row) %>%
     dplyr::mutate(max_value = max(value),
-           group_number = stringr::str_extract(name,"[:digit:]") %>% as.numeric(),
-           GeneCluster = dplyr::if_else(value == max_value ,group_number,NA_real_)) %>%
+                  group_number = stringr::str_extract(name,"[:digit:]") %>% as.numeric(),
+                  GeneCluster = dplyr::if_else(value == max_value ,group_number,NA_real_)) %>%
     tidyr::fill(GeneCluster,.direction = c("updown")) %>%
     dplyr::select(-group_number,-max_value) %>%
     tidyr::pivot_wider(names_from = name,values_from = value)
+  
+  P.value = sig$P.Value
+  sig2 = data.frame(sig2,P.value)
+
   #tidy to a dataframe
-  sig1 <- as.data.frame(sig1)
-  sig1 = sig1[,-1]
-  rownames(sig1) = rownames(sig);
+  sig2 <- as.data.frame(sig2)
+  sig2 = sig2[,-1] #remove 'row' column
+  rownames(sig2) = rownames(sig)
   
   #write the results as the csv file
-  write.table(sig1,"specific_subtype_gene.txt",sep = "\t", quote = FALSE)
+  write.table(sig2,"specific_subtype_gene.txt",sep = "\t", quote = FALSE)
   
   #return the results
-  print(head(sig1))
+  print(head(sig2))
   
   #warning
-  print(writeLines("\nNOTE: \n*the results, which are being shown above, are incomplete.\n*specific_subtype_gene.csv placed in your current working directory.\n*Please check to identify which gene is specifically assigned to which subgroup."))
+  print(writeLines("\nNOTE: \n*the results shown above are incomplete.\n*specific_subtype_gene.csv placed in your current working directory.\n*Please check to identify which gene is specifically assigned to which subgroup."))}
 }
